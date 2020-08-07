ec63ed260c3666d5d9e317d30ed52d069b25f2f2
DESKTOP-A99IIOS\Simon Hempel <hempel-costello.simon20@libertycommon.org>
2020-08-07 17:52:43 -0600

    some improvements, but no real progresss

--- SwerveBot/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ SwerveBot/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -7,6 +7,8 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
@@ -36,16 +38,20 @@ public class SwerveModule extends SubsystemBase {
   private double currVal;
   private double inputPos;
   private double outputPos;
-  private double upVal;
-  private double downVal;
-  private double upRevVal;
-  private double downRevVal;
-  private double upValDif;
-  private double downValDif;
+  private double upInput;
+  private double downInput;
+  private double upRevInput;
+  private double downRevInput;
+  private double upDif;
+  private double downDif;
   private double upRevDif;
   private double downRevDif;
+  private double currentAbsPosition;
+  private double currentAbsAngle;
+  private double inputPosAngle;
   private double lastThrottleDirection;
-  private boolean shouldReverse;
+  private ArrayList<Double> difArrayList = new ArrayList<Double>();
+
   private double modOffset;
   public SwerveModule(double P, double I, double D, CANSparkMax turnMotor, CANSparkMax driveMotor, Counter absoluteEncoder, double absEncoderOffset ) {
     kP = P;
@@ -73,68 +79,52 @@ public class SwerveModule extends SubsystemBase {
     SmartDashboard.putNumber("setPos", 0);
   }
   public void setOutput(Vector inputVector){
-    inputPos = (inputVector.getVectorAngleToi())/(2*Math.PI)-0.5;
-    shouldReverse = false;
-
-    upVal = (Math.ceil(currVal)+inputPos);
-    upValDif = Math.abs(currVal-upVal);
-
-    downVal = (Math.floor(currVal)+inputPos);
-    downValDif = Math.abs(currVal-downVal);
-
-    upRevVal = upVal + 0.5;
-    upRevDif = Math.abs(currVal - upRevVal);
-
-    downRevVal = downVal+0.5;
-    downRevDif = Math.abs(currVal-downRevVal);
-    System.out.println("upRevDif" + upRevDif);
-    System.out.println("downDif" + downRevDif);
-    System.out.println("upDif"+upValDif);
-    System.out.println("downRevDif" + downValDif);
-    if(upValDif<downValDif){
-      if(upValDif>=downRevDif){
-        outputPos = downRevVal;
-        shouldReverse = true;
-        
-        modOffset = 0.5;
-        SmartDashboard.putString("valString", "upRevVal");
-
-      }
-      else{
-        shouldReverse = false;
-        modOffset = 0;
-        outputPos = upVal;
-        SmartDashboard.putString("valString", "upVal");
-      }
+    inputPos = (inputVector.getVectorAngleToi())/(2*Math.PI);
+    upInput = inputPos;
+    downInput  = -inputPos;
+    upRevInput = inputPos -0.5;
+    downRevInput = -inputPos + 0.5;
+    currentAbsPosition  = currVal%1;
+    upDif = upInput-currentAbsPosition;
+    downDif = downInput-currentAbsPosition;
+    upRevDif = upRevInput - currentAbsPosition;
+    downRevDif = downRevInput-currentAbsPosition;
+    
+    if(Math.abs(upDif)<Math.abs(downDif)&&Math.abs(upDif)<Math.abs(upRevDif)&&Math.abs(upDif)<Math.abs(downRevDif)){
+      currVal = currVal + upDif;
+    }
+    else if(Math.abs(downDif)<Math.abs(upRevDif)&&Math.abs(downDif)<Math.abs(downRevDif)){
+      currVal = currVal +downDif;
+    }
+    else if(Math.abs(upRevDif)<Math.abs(downRevDif)){
+      currVal = currVal + upRevDif;
     }
     else{
-      if(downValDif>=upRevDif){
-        outputPos = upRevVal;
-        shouldReverse = true;
-        modOffset = -0.5;
-        SmartDashboard.putString("valString", "downRevVal");
-      }
-      else{
-        shouldReverse = false;
-        outputPos = downVal;
-        modOffset = 0;
-        SmartDashboard.putString("valString", "downVal");
-
-      }
+      currVal = currVal + downRevDif;
     }
+    /*SmartDashboard.putNumber("upInput", upInput);
+    SmartDashboard.putNumber("downInput", downInput);
+    SmartDashboard.putNumber("upRevInput", upRevInput);
+    SmartDashboard.putNumber("downRevInput", downRevInput);
+    SmartDashboard.putNumber("currVal", currentAbsPosition);
+    SmartDashboard.putNumber("upDif", upDif);
+    SmartDashboard.putNumber("downDif", downDif);*/
+    currentAbsAngle = Math.abs((currVal%1)*2*Math.PI);
+    inputPosAngle = Math.abs(inputPos*2*Math.PI);
+    SmartDashboard.putBoolean("rev", Math.abs(currentAbsAngle-inputPosAngle)>=180);
+    SmartDashboard.putNumber("currentAbleAngle",currentAbsAngle );
+    SmartDashboard.putNumber("inputPosAngle", inputPosAngle);
+    if(Math.abs(currentAbsAngle-inputPosAngle)>90){
+      setThrottle(inputVector.magnitude()*0.25, true);
+    }
+    else{
+      setThrottle(inputVector.magnitude()*0.25, false);
 
-    SmartDashboard.putNumber("upval", upVal);
-    SmartDashboard.putNumber("upRevval", upRevVal);
-    SmartDashboard.putNumber("downVal", downVal);
-    SmartDashboard.putNumber("downRevVal", downRevVal);
-    SmartDashboard.putNumber("currVall", currVal);
-    SmartDashboard.putNumber("inputPos", inputPos);
-    SmartDashboard.putNumber("outputPos", outputPos);
-    SmartDashboard.putBoolean("shouldReverse", shouldReverse);
-    setAngle(outputPos);
-    //setThrottle(inputVector.magnitude()*0.25, shouldReverse);
-    
-    currVal = outputPos+modOffset;
+    }
+   
+
+    setAngle(currVal);
+ 
   }
   public double getAbsEncoderValue(){
     return aEncoder.getPeriod()*-1000000;
@@ -143,20 +133,18 @@ public class SwerveModule extends SubsystemBase {
     mpidController.setReference(desiredPosition, ControlType.kPosition);
   }
   public void setThrottle(double desiredThrottle, boolean reverse){
-    lastThrottleDirection = Math.signum(dMotor.get());
-    if(lastThrottleDirection!=0){
-      desiredThrottle = desiredThrottle*lastThrottleDirection;
-      if(reverse){
-        desiredThrottle = desiredThrottle*-1;
-      }   
+    if(reverse){
+      dMotor.set(-desiredThrottle);
     }
-    dMotor.set(desiredThrottle);
+    else{
+      dMotor.set(desiredThrottle);
+    }
   }
   public double getAngle(){
     return tMotor.getEncoder().getPosition();
   }
   public void teleopPeriodic(){
-    outputVector.setX(RobotMap.buttonMap.driveStickSideways());
+    outputVector.setX(-RobotMap.buttonMap.driveStickSideways());
     outputVector.setY(RobotMap.buttonMap.driveStickUP());
 
     setOutput(outputVector);
--- SwerveBot/src/main/java/frc/robot/tools/math/Vector.java
+++ SwerveBot/src/main/java/frc/robot/tools/math/Vector.java
@@ -33,7 +33,7 @@ public class Vector {
 	  yVec = y;
    }
    public double getVectorAngleToi(){
-      if(Math.abs(xVec)<0.8 && Math.abs(yVec)<0.8){
+      if(Math.abs(xVec)<0.05 && Math.abs(yVec)<0.05){
          return lastAngle;
       }
       else{
